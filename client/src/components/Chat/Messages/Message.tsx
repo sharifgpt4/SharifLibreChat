1c29d1f53545d0b96e72aa37f3b5f3bcfa6f1bb0
bachetesla <root@bachetesla.ir>
2024-07-26 13:29:58 +0330

    Update

--- client/src/components/Chat/Messages/Message.tsx
+++ client/src/components/Chat/Messages/Message.tsx
@@ -5,64 +5,18 @@ import MessageRender from './ui/MessageRender';
 // eslint-disable-next-line import/no-cycle
 import MultiMessage from './MultiMessage';
 
-<<<<<<< HEAD
-export default function Message(props: TMessageProps) {
-  const UsernameDisplay = useRecoilValue<boolean>(store.UsernameDisplay);
-  const { user } = useAuthContext();
-  const localize = useLocalize();
-
-  const {
-    ask,
-    icon,
-    edit,
-    isLast,
-    enterEdit,
-    handleScroll,
-    conversation,
-    isSubmitting,
-    latestMessage,
-    handleContinue,
-    copyToClipboard,
-    regenerateMessage,
-  } = useMessageHelpers(props);
-
-  const { message, siblingIdx, siblingCount, setSiblingIdx, currentEditId, setCurrentEditId } =
-    props;
-
-  if (!message) {
-    return null;
-  }
-
-  const { text, children, messageId = null, isCreatedByUser, error, unfinished } = message ?? {};
-
-  let messageLabel = '';
-  if (isCreatedByUser) {
-    messageLabel = UsernameDisplay ? user?.name : localize('com_user_message');
-  } else {
-    if (message.sender === 'GPT-4'){
-      messageLabel = 'GPT-4.5';
-    }else {
-      messageLabel = message.sender;
-    }
-
-  }
-
-  return (
-    <>
-=======
 const MessageContainer = React.memo(
-  ({ handleScroll, children }: { handleScroll: () => void; children: React.ReactNode }) => {
-    return (
->>>>>>> e5dfa06e6ce11e7cf4e11c400ec028dc8ee3600d
-      <div
-        className="text-token-text-primary w-full border-0 bg-transparent dark:border-0 dark:bg-transparent"
-        onWheel={handleScroll}
-        onTouchMove={handleScroll}
-      >
-        {children}
-      </div>
-    );
-  },
+    ({ handleScroll, children }: { handleScroll: () => void; children: React.ReactNode }) => {
+      return (
+          <div
+              className="text-token-text-primary w-full border-0 bg-transparent dark:border-0 dark:bg-transparent"
+              onWheel={handleScroll}
+              onTouchMove={handleScroll}
+          >
+            {children}
+          </div>
+      );
+    },
 );
 
 export default function Message(props: TMessageProps) {
@@ -83,40 +37,40 @@ export default function Message(props: TMessageProps) {
   const { children, messageId = null } = message ?? {};
 
   return (
-    <>
-      <MessageContainer handleScroll={handleScroll}>
-        {showSibling ? (
-          <div className="m-auto my-2 flex justify-center p-4 py-2 text-base md:gap-6">
-            <div className="flex w-full flex-row flex-wrap justify-between gap-1 md:max-w-5xl md:flex-nowrap md:gap-2 lg:max-w-5xl xl:max-w-6xl">
-              <MessageRender
-                {...props}
-                message={message}
-                isSubmittingFamily={isSubmittingFamily}
-                isCard
-              />
-              <MessageRender
-                {...props}
-                isMultiMessage
-                isCard
-                message={siblingMessage ?? latestMultiMessage ?? undefined}
-                isSubmittingFamily={isSubmittingFamily}
-              />
-            </div>
-          </div>
-        ) : (
-          <div className="m-auto justify-center p-4 py-2 text-base md:gap-6 ">
-            <MessageRender {...props} />
-          </div>
-        )}
-      </MessageContainer>
-      <MultiMessage
-        key={messageId}
-        messageId={messageId}
-        conversation={conversation}
-        messagesTree={children ?? []}
-        currentEditId={currentEditId}
-        setCurrentEditId={setCurrentEditId}
-      />
-    </>
+      <>
+        <MessageContainer handleScroll={handleScroll}>
+          {showSibling ? (
+              <div className="m-auto my-2 flex justify-center p-4 py-2 text-base md:gap-6">
+                <div className="flex w-full flex-row flex-wrap justify-between gap-1 md:max-w-5xl md:flex-nowrap md:gap-2 lg:max-w-5xl xl:max-w-6xl">
+                  <MessageRender
+                      {...props}
+                      message={message}
+                      isSubmittingFamily={isSubmittingFamily}
+                      isCard
+                  />
+                  <MessageRender
+                      {...props}
+                      isMultiMessage
+                      isCard
+                      message={siblingMessage ?? latestMultiMessage ?? undefined}
+                      isSubmittingFamily={isSubmittingFamily}
+                  />
+                </div>
+              </div>
+          ) : (
+              <div className="m-auto justify-center p-4 py-2 text-base md:gap-6 ">
+                <MessageRender {...props} />
+              </div>
+          )}
+        </MessageContainer>
+        <MultiMessage
+            key={messageId}
+            messageId={messageId}
+            conversation={conversation}
+            messagesTree={children ?? []}
+            currentEditId={currentEditId}
+            setCurrentEditId={setCurrentEditId}
+        />
+      </>
   );
 }
--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -1,109 +1,41 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import { useSearchParams } from 'react-router-dom'; // Add this line to use search params
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { useGetModelsQuery, useGetSearchEnabledQuery } from 'librechat-data-provider/react-query';
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+
 import type { ContextType } from '~/common';
-import {
-  useAuthContext,
-  useServerStream,
-  useConversation,
-  useAssistantsMap,
-  useFileMap,
-} from '~/hooks';
-import { AssistantsMapContext, FileMapContext } from '~/Providers';
+import { useAuthContext, useAssistantsMap, useFileMap, useSearch } from '~/hooks';
+import { AssistantsMapContext, FileMapContext, SearchContext } from '~/Providers';
 import { Nav, MobileNav } from '~/components/Nav';
-import store from '~/store';
-import PaymentStatusModal from '~/components/Nav/PaymentStatus'; // Make sure the path is correct
 
 export default function Root() {
-  const location = useLocation();
-  const [searchParams] = useSearchParams();
-  const { newConversation } = useConversation();
   const { isAuthenticated } = useAuthContext();
   const [navVisible, setNavVisible] = useState(() => {
     const savedNavVisible = localStorage.getItem('navVisible');
     return savedNavVisible !== null ? JSON.parse(savedNavVisible) : true;
   });
-  const [isPaymentModalOpen, setPaymentModalOpen] = useState(false);
-  const [paymentSuccess, setPaymentSuccess] = useState('');
-  const [paymentTrackId, setPaymentTrackId] = useState('');
 
-  const submission = useRecoilValue(store.submission);
-  useServerStream(submission ?? null);
-
-  const modelsQueryEnabled = useRecoilValue(store.modelsQueryEnabled);
-  const setIsSearchEnabled = useSetRecoilState(store.isSearchEnabled);
-  const setModelsConfig = useSetRecoilState(store.modelsConfig);
-
+  const search = useSearch({ isAuthenticated });
   const fileMap = useFileMap({ isAuthenticated });
   const assistantsMap = useAssistantsMap({ isAuthenticated });
-  const searchEnabledQuery = useGetSearchEnabledQuery({ enabled: isAuthenticated });
-  const modelsQuery = useGetModelsQuery({ enabled: isAuthenticated && modelsQueryEnabled });
-
-  useEffect(() => {
-    localStorage.setItem('navVisible', JSON.stringify(navVisible));
-  }, [navVisible]);
-
-  useEffect(() => {
-    const paymentSuccessParam = searchParams.get('Payment_success');
-    const paymentTrackIdParam = searchParams.get('Payment_trackId');
-
-    if (paymentSuccessParam !== null && paymentTrackIdParam !== null) {
-      setPaymentModalOpen(true);
-      setPaymentSuccess(paymentSuccessParam);
-      setPaymentTrackId(paymentTrackIdParam);
-    }
-  }, [location.search]); // This will trigger the effect whenever the search part of the URL changes
-
-  useEffect(() => {
-    if (modelsQuery.data && location.state?.from?.pathname.includes('/chat')) {
-      setModelsConfig(modelsQuery.data);
-      newConversation({}, undefined, modelsQuery.data);
-    } else if (modelsQuery.data) {
-      setModelsConfig(modelsQuery.data);
-    } else if (modelsQuery.isError) {
-      console.error('Failed to get models', modelsQuery.error);
-    }
-  }, [modelsQuery.data, modelsQuery.isError]);
-
-  useEffect(() => {
-    if (searchEnabledQuery.data) {
-      setIsSearchEnabled(searchEnabledQuery.data);
-    } else if (searchEnabledQuery.isError) {
-      console.error('Failed to get search enabled', searchEnabledQuery.error);
-    }
-  }, [searchEnabledQuery.data, searchEnabledQuery.isError]);
 
   if (!isAuthenticated) {
     return null;
   }
 
-  // Function to handle closing the modal
-  const handleCloseModal = () => {
-    setPaymentModalOpen(false);
-  };
-
   return (
-    <FileMapContext.Provider value={fileMap}>
-      <AssistantsMapContext.Provider value={assistantsMap}>
-        <div className="flex h-dvh">
-          <div className="relative z-0 flex h-full w-full overflow-hidden">
-            <Nav navVisible={navVisible} setNavVisible={setNavVisible} />
-            <div className="relative flex h-full max-w-full flex-1 flex-col overflow-hidden">
-              <MobileNav setNavVisible={setNavVisible} />
-              <Outlet context={{ navVisible, setNavVisible } satisfies ContextType} />
-              <PaymentStatusModal
-                isOpen={isPaymentModalOpen}
-                onClose={handleCloseModal}
-                success={paymentSuccess === '1'}
-                trackId={paymentTrackId}
-              />
+      <SearchContext.Provider value={search}>
+        <FileMapContext.Provider value={fileMap}>
+          <AssistantsMapContext.Provider value={assistantsMap}>
+            <div className="flex h-dvh">
+              <div className="relative z-0 flex h-full w-full overflow-hidden">
+                <Nav navVisible={navVisible} setNavVisible={setNavVisible} />
+                <div className="relative flex h-full max-w-full flex-1 flex-col overflow-hidden">
+                  <MobileNav setNavVisible={setNavVisible} />
+                  <Outlet context={{ navVisible, setNavVisible } satisfies ContextType} />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </AssistantsMapContext.Provider>
-    </FileMapContext.Provider>
+          </AssistantsMapContext.Provider>
+        </FileMapContext.Provider>
+      </SearchContext.Provider>
   );
 }
